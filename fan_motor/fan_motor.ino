a90b77b6cb6a6364ba32ad3fd0127db30898828b
orihara <b1015030@fun.ac.jp>
2018-05-31 17:02:33 +0900

    とりあえずモーターが回るだけのサンプル

--- fan_motor/fan_motor.ino
+++ fan_motor/fan_motor.ino
@@ -1,9 +1,15 @@
-void setup() {
-  // put your setup code here, to run once:
-
+void setup(){
+  pinMode(1,OUTPUT); //信号用ピン 6番ピン
+  pinMode(2,OUTPUT); //信号用ピン 5番ピン
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop(){
+  int val = 200;
 
+  // 5番ピンをHIGH, 6番ピンをLOW
+  digitalWrite(1,LOW);
+  digitalWrite(2,HIGH);
+
+  //valが大きいほど出力値も大きくなる
+  analogWrite( 3 , val ); //出力値:1~255
 }
