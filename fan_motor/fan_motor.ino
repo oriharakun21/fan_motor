9793e0cba7936d748d19577f735d0178d161b84e
orihara <b1015030@fun.ac.jp>
2018-06-12 14:08:10 +0900

    バウンジング対策をしたスイッチのオンオフを実装

--- fan_motor/fan_motor.ino
+++ fan_motor/fan_motor.ino
@@ -10,22 +10,36 @@ const int m_signal2P = 3;
 const int m_speedP = 4;
 // そのた
 const int in_m_controlP = 5;
+// モーター関連
+int m_val;  //出力値:1~255
+// スイッチ
+int state; // 0 オフ 1　オン
+int s_val, s_old_val;
 
 void setup(){
   pinMode(m_signal1P,OUTPUT);
   pinMode(m_signal2P,OUTPUT);
   pinMode(in_m_controlP,INPUT);
+  m_val = 255;
 }
 
 void loop(){
-  int val = 180;
-
   digitalWrite(m_signal1P,HIGH);
   digitalWrite(m_signal2P,LOW);
-
-  if(digitalRead(in_m_controlP) == HIGH){
-    analogWrite(m_speedP, val); //出力値:1~255
-  }else{
-    analogWrite(m_speedP, 0); //出力値:1~255
+  swtchChange();
+}
+// スイッチ（バウンジング対策済み）
+void swtchChange(){
+  s_val = digitalRead(in_m_controlP);
+  if(s_val == HIGH && s_old_val == LOW){
+    state = 1 - state;
+    delay(10);
+  }
+  s_old_val = s_val;
+  if(state == 1){
+    analogWrite(m_speedP, m_val);
+  } else {
+    analogWrite(m_speedP, 0);
   }
 }
+
