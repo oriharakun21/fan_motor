0f6078197bc89e8702b87d83803092799a430f76
orihara <b1015030@fun.ac.jp>
2018-06-27 18:59:56 +0900

    bluetoothを使うためのコードに（動かない）

--- fan_motor/fan_motor.ino
+++ fan_motor/fan_motor.ino
@@ -1,44 +1,32 @@
-/*
- * 2018/6/6 オリハラ
- * fan_motor
- * モーターを回転させるだけのシステム
- */
- 
 // モーター用ピン
 const int m_signal1P = 2;
 const int m_signal2P = 3;
 const int m_speedP = 4;
-// そのた
-const int in_m_controlP = 5;
 // モーター関連
 int m_val;  //出力値:1~255
-// スイッチ
-int state; // 0 オフ 1　オン
-int s_val, s_old_val;
 
 void setup(){
+  Serial.begin(115200);
+  Serial.println("Ready");
   pinMode(m_signal1P,OUTPUT);
   pinMode(m_signal2P,OUTPUT);
-  pinMode(in_m_controlP,INPUT);
   m_val = 255;
 }
 
 void loop(){
   digitalWrite(m_signal1P,HIGH);
   digitalWrite(m_signal2P,LOW);
-  swtchChange();
+  switchSerial();
+  Serial.print(Serial.read());
 }
-// スイッチ（バウンジング対策済み）
-void swtchChange(){
-  s_val = digitalRead(in_m_controlP);
-  if(s_val == HIGH && s_old_val == LOW){
-    state = 1 - state;
-    delay(10);
-  }
-  s_old_val = s_val;
-  if(state == 1){
-    analogWrite(m_speedP, m_val);
-  } else {
-    analogWrite(m_speedP, 0);
+
+void switchSerial(){
+  if(Serial.available()){
+    int tmp=Serial.read();
+    if(tmp == 1){
+        analogWrite(m_speedP, m_val);
+      } else {
+        analogWrite(m_speedP, 0);
+      }
   }
 }
--- fun_switch/fun_switch.ino
+++ fun_switch/fun_switch.ino
@@ -1,7 +1,24 @@
+// スイッチ
+int state; // 0 オフ 1　オン
+int s_val, s_old_val;
+int in_m_controlP=3;
+
 void setup() {
   Serial.begin(115200);
+  pinMode(in_m_controlP,INPUT);
 }
 
 void loop() {
-  Serial.print();
+  swtchChange();
+  Serial.print(state);
+}
+
+// スイッチ（バウンジング対策済み）
+void swtchChange(){
+  s_val = digitalRead(in_m_controlP);
+  if(s_val == HIGH && s_old_val == LOW){
+    state = 1 - state;
+    delay(10);
+  }
+  s_old_val = s_val;
 }
